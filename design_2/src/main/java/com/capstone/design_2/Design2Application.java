546b00943aacd17bfb485a01e6ca55e0e1e06476
baeksunghyun <ces0135@naver.com>
2025-03-28 18:20:54 +0900

    Project Setting

---
+++ design_2/src/main/java/com/capstone/design_2/Design2Application.java
@@ -0,0 +1,13 @@
+package com.capstone.design_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Design2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Design2Application.class, args);
+	}
+
+}
---
+++ design_2/src/test/java/com/capstone/design_2/Design2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.design_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Design2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
