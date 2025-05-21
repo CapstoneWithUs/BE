676cc5ed42b1cbea7a4f252866b0c5f30f70dcf1
baeksunghyun <ces0135@naver.com>
2025-05-22 01:51:10 +0900

    Fix: 배포용 WebConfig 수정

--- src/main/java/capstondesign2/backend/config/WebConfig.java
+++ src/main/java/capstondesign2/backend/config/WebConfig.java
@@ -17,9 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://pocusmate.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
 }
